d5ddc8094963702805b3c022402cab19f9179532
pankajjadhaopp23 <pankajjadhaopp23@gmail.com>
2026-08-16 23:57:08 +0530

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
 
-android { namespace = "com.kautuk.owner"; compileSdk = 35
-    defaultConfig { applicationId = "com.kautuk.owner"; minSdk = 23; targetSdk = 35; versionCode = 1; versionName = "1.0" }
+android { namespace = "com.kautuk.partner"; compileSdk = 35
+    defaultConfig { applicationId = "com.kautuk.partner"; minSdk = 23; targetSdk = 35; versionCode = 1; versionName = "1.0" }
 }
